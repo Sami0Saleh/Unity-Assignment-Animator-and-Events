8462668c10a84d4a157c363a5dbfde5db4552e39
Sami0Saleh <sami.saleh.87@gmail.com>
2024-02-23 14:11:37 +0200

    updated jump

--- Animator and Events/Assets/Scripts/AiControll.cs
+++ Animator and Events/Assets/Scripts/AiControll.cs
@@ -16,39 +16,39 @@ public class AiControll : MonoBehaviour
     private bool Jumping = false;
     private float jumpHeight = 2.0f;
     private float jumpDuration = 1f;
-    private float timeElapsed = 0;
     private void Start()
     {
         navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
     }
-
     private void Update()
     {
-
-        if (navMeshAgent.isOnOffMeshLink)
+        if (navMeshAgent.isOnOffMeshLink && !Jumping)
         {
-            Jumping = true;
+            StartCoroutine(Jump());
         }
+    }
+    IEnumerator Jump()
+    {
+        Jumping = true;
 
-        if (Jumping == true)
+        OffMeshLinkData data = navMeshAgent.currentOffMeshLinkData;
+        Vector3 startPos = navMeshAgent.transform.position;
+        Vector3 endPos = data.endPos;
+
+        float timeElapsed = 0f;
+
+        while (timeElapsed < jumpDuration)
         {
-            OffMeshLinkData data = navMeshAgent.currentOffMeshLinkData;
-            Vector3 startPos = data.startPos;
-            Vector3 endPos = data.endPos;
-
-            if (timeElapsed < jumpDuration)
-            {
-                float t = timeElapsed / jumpDuration;
-                float yOffset = jumpHeight * 4.0f * (t - t * t);
-                navMeshAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + yOffset * Vector3.up;
-                timeElapsed += Time.deltaTime / jumpDuration;
-            }
-            else
-            {
-                navMeshAgent.transform.position = data.endPos;
-                Jumping = false;
-
-            }
+            float t = timeElapsed / jumpDuration;
+            float yOffset = jumpHeight * 4.0f * (t - t * t);
+            navMeshAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + yOffset * Vector3.up;
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
+        
+        navMeshAgent.transform.position = endPos;
+        Jumping = false;
+
+        navMeshAgent.CompleteOffMeshLink();
     }
 }
